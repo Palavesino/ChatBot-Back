54e5cd42f1353d5fef2268a3b8cf45376aff3bb1
Facundo <palavesinofacundo@gmail.com>
2024-11-26 13:27:05 -0300

    Refactoring code

--- app.js
+++ app.js
@@ -2,64 +2,32 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
-const { createBot, createProvider, createFlow, EVENTS,addKeyword } = require('@bot-whatsapp/bot')
-const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MockAdapter = require('@bot-whatsapp/database/mock')
-require('dotenv').config()
+const { createBot, createProvider, createFlow, EVENTS, addKeyword } = require('@bot-whatsapp/bot');
+const BaileysProvider = require('@bot-whatsapp/provider/baileys');
+const MockAdapter = require('@bot-whatsapp/database/mock');
+require('dotenv').config();
+
 const app = express();
-const port = process.env.PORT || 3000; 
 app.use(cors());
 
 const flowMenu = addKeyword(EVENTS.WELCOME)
-.addAnswer('ᴡ ᴇ ʟ ᴄ ᴏ ᴍ ᴇ  𝓣𝓸  𝓒𝓱𝓪𝓽𝓑𝓸𝓽 The New WORLD')
-
+  .addAnswer('ᴡ ᴇ ʟ ᴄ ᴏ ᴍ ᴇ  𝓣𝓸  𝓒𝓱𝓪𝓽𝓑𝓸𝓽 The New WORLD');
 
 const main = async () => {
-  const adapterDB = new MockAdapter()
-  const adapterFlow = createFlow([flowMenu])
-  const adapterProvider = createProvider(BaileysProvider)
+  const adapterDB = new MockAdapter();
+  const adapterFlow = createFlow([flowMenu]);
+  const adapterProvider = createProvider(BaileysProvider);
 
   createBot({
     flow: adapterFlow,
     provider: adapterProvider,
     database: adapterDB,
-  })
-}
+  });
+};
 
-// // Función para convertir un archivo en base64
-// const convertToBase64 = (filePath) => {
-//     try {
-//         const file = fs.readFileSync(filePath);  // Lee el archivo de forma síncrona
-//         return file.toString('base64');  // Convierte a Base64
-//     } catch (error) {
-//         console.error("Error al leer el archivo:", error);
-//         throw error;
-//     }
-// };
-
-// // Endpoint para ejecutar el método main y devolver la imagen en base64
-// app.get('/start-bot', async (req, res) => {
-//     try {
-//         console.log("Generando qr")
-//         // Ejecuta el método main al hacer un fetch a este endpoint
-//         await main();
-
-//         // Ruta al archivo PNG generado
-//         const imagePath = path.join(process.cwd(), 'bot.qr.png');
-
-//         // Convierte la imagen a base64
-//         const imageBase64 = convertToBase64(imagePath);
-
-//         // Devuelve la imagen en base64 y un mensaje
-//         res.status(200).json({
-//             message: 'Bot iniciado correctamente.',
-//             imageBase64: imageBase64,
-//         });
-//     } catch (error) {
-//         res.status(500).json({ error: 'Ocurrió un error al iniciar el bot.' });
-//     }
-// });
-
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.get('/start-bot', async (req, res) => {
   try {
@@ -81,17 +49,11 @@ app.get('/get-qr', async (req, res) => {
     const image = fs.createReadStream(imagePath);
     res.setHeader('Content-Type', 'image/png');
     res.setHeader('Content-Disposition', 'attachment; filename="bot.qr.png"');
-    // Envía el archivo directamente
     image.pipe(res);
   } catch (error) {
     res.status(500).json({ error: 'Ocurrió un error al iniciar el bot.' });
   }
 });
 
-
-
-
-// Arrancar el servidor de Express
-app.listen(port, () => {
-  console.log(`Servidor Express corriendo en http://localhost:${port}`);
-});
+// Exportar la aplicación en lugar de iniciar el servidor
+module.exports = app;
